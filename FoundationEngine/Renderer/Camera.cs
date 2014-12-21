b835ffab5b9befbc798524373ee4f70aa35aed4f
Edwin Jones <edwinj85@gmail.com>
2014-12-21 17:05:29 +0000

    cleaning up solution

---
+++ FoundationEngine/Renderer/Camera.cs
@@ -0,0 +1,13 @@
+﻿using SharpDX;
+
+namespace FoundationEngine.Renderer
+{
+    /// <summary>
+    /// This class represents a camera, or view of the 3D scene.
+    /// </summary>
+    public class Camera
+    {
+        public Vector3 Position { get; set; }
+        public Vector3 Target { get; set; }
+    }  
+}
---
+++ FoundationEngine/Renderer/Mesh.cs
@@ -0,0 +1,31 @@
+﻿using SharpDX;
+using System;
+
+namespace FoundationEngine.Renderer
+{
+    /// <summary>
+    /// This class represents a 3D mesh.
+    /// </summary>
+    class Mesh
+    {
+        public String Name { get; set; }
+        public Vector3[] Vertices { get; private set; }
+        public Face[] Faces { get; set; }
+        public Vector3 Position { get; set; }
+        public Vector3 Rotation { get; set; }
+
+        public Mesh(String name, Int32 verticesCount, Int32 facesCount)
+        {
+            Vertices = new Vector3[verticesCount];
+            Faces = new Face[facesCount];
+            Name = name;
+        }
+
+        public Mesh(String name, Vector3[] verts, Face[] faces)
+        {
+            Vertices = verts;
+            Faces = faces;
+            Name = name;
+        }
+    }
+}
